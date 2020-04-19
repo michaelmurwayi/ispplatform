9ce78b236689bbfe10c32531b960b00d6c77e613
michaelmurwayi <https://github.com/michaelmurwayi>
2020-04-19 18:33:53 +0300

    user account template draft

--- useraccess/urls.py
+++ useraccess/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView, SignupView
+from .views import HomeView, SignupView, ProfileView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home" ),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('signup/', SignupView.as_view(), name="signup")
+    path('signup/', SignupView.as_view(), name="signup"),
+    path('profile/', ProfileView.as_view(), name="profile")
 ]
 
