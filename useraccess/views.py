3c4291b24eeccb063a7bf1f8191a9ffe0d1347b7
michaelmurwayi <https://github.com/michaelmurwayi>
2020-05-08 23:25:22 +0300

    user signup: sorting out user registration process

--- useraccess/views.py
+++ useraccess/views.py
@@ -7,6 +7,7 @@ from .models import Radacct
 from django.views.generic import ListView
 from django.views.generic.detail import SingleObjectMixin
 from .forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -21,23 +22,22 @@ class SignupView(CreateView):
     success_url = reverse_lazy('profile')
     template_name = "signup.html"
 
-    def post(self, request):
-        import ipdb; ipdb.set_trace()
-        pass
+    # def post(self, request):
+    #     import ipdb; ipdb.set_trace()
+    #     pass
 
 class ProfileView(SingleObjectMixin, ListView):
     # view for the user profile page 
 
     template_name = 'profile.html'
     form_class = UserChangeForm
+    user = User
+    
 
     def get(self, request):
-        # handling page get request 
-
+        # handling page get request
         account_detail = Radacct.objects.get(username= "tony") # query to access user database information
         context = {
-            "username": request.user.username,
-            "email": request.user.email, 
             "session_start_time": account_detail.connectinfo_start,
             "session_stop_time": account_detail.connectinfo_stop,
         }
