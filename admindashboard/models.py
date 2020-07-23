b41447ec466771a915e7ed2afa0bc017e9067a04
kenngare <kenngare98@gmail.com>
2020-07-23 18:12:09 +0300

    Dashboard

---
+++ admindashboard/models.py
@@ -0,0 +1,26 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Messages(models.Model):
+
+    message = models.TextField(max_length=100)
+
+
+class UseraccessSelectedpackages(models.Model):
+    bundle = models.CharField(max_length=10)
+    speed = models.CharField(max_length=10)
+    expiry = models.CharField(db_column='Expiry', max_length=50)  # Field name made lowercase.
+    balance = models.CharField(max_length=100)
+    access_period = models.CharField(max_length=10)
+    username = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'useraccess_selectedpackages'
